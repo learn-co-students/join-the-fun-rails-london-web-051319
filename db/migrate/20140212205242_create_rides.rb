781e9b5c6713a3e7119348db7c0863d94f62d79a
mbarrins <maija.barrins@gmail.com>
2019-06-07 14:26:50 +0100

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.integer :taxi_id, index: true
+      t.integer :passenger_id, index: true
       t.timestamps null: false
     end
   end
